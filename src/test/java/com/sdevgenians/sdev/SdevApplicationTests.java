99a1ad608faeeaab2ed74f6e68da70edb28fbe17
잉퓨 <dayeon620@kakao.com>
2023-11-01 00:20:42 +0900

    Initial commit

---
+++ src/test/java/com/sdevgenians/sdev/SdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdevgenians.sdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
